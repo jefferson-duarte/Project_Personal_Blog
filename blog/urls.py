470ea28a76c06ccb941fe9eed0f5f8e74af0c030
Jefferson Duarte <jeffersonsduarte@live.com>
2024-06-12 10:43:16 +0100

    Created function to render each post and created_by

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('page/', views.page, name='page'),
     path('post/<slug:slug>/', views.post, name='post'),
+    path('page/<slug:slug>/', views.page, name='page'),
+    path('created_by/<int:author_pk>/', views.created_by, name='created_by'),
+    path('category/<slug:slug>/', views.category, name='category'),
 ]
